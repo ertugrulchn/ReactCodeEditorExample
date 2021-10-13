0485ad8bcbb0c8594d9cf0952285fcd632821e6f
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-10-14 01:03:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { EditorProvider } from "context";
+import CodeBar from "../src/components/CodeBar";
+import Preview from "../src/components/Preview";
+import Header from "../src/components/Header";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EditorProvider>
+      <CodeBar />
+      <main className={styles.main}>
+        <Header />
+        <Preview />
+      </main>
+    </EditorProvider>
   );
 }
 
---
+++ src/components/Editor/JsEditor.js
@@ -0,0 +1,36 @@
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/snippets/javascript";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-min-noconflict/ext-language_tools";
+import { EditorContext, useContext } from "context";
+
+function JsEditor() {
+  const { js, setJs } = useContext(EditorContext);
+
+  return (
+    <AceEditor
+      placeholder="JS kodlarınızı buraya yazın!"
+      mode="javascript"
+      theme="monokai"
+      name="editor_js"
+      fontSize={16}
+      value={js}
+      height={"100%"}
+      width={"100%"}
+      onChange={(val) => setJs(val)}
+      showPrintMargin={true}
+      showGutter={false}
+      highlightActiveLine={true}
+      setOptions={{
+        enableBasicAutocompletion: true,
+        enableLiveAutocompletion: true,
+        enableSnippets: true,
+        showLineNumbers: false,
+        tabSize: 2,
+      }}
+    />
+  );
+}
+
+export default JsEditor;
